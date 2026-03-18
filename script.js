9712016038291772d81bb858d3430e0893397710
Elasinus <ghalepritam2001@gmail.com>
2026-03-18 01:41:33 -0400

    added

--- script.js
+++ script.js
@@ -1,39 +1,87 @@
 let humanScore = 0;
 let computerScore = 0;
 
+let z = document.createElement("IMG")
+z.setAttribute('width', '373px')
+z.setAttribute('height', '248px')
+z.style.display = "block"
+z.style.marginLeft = "auto"
+z.style.marginRight = "auto"
+
+let x = document.createElement("IMG")
+x.setAttribute('width', '373px')
+x.setAttribute('height', '248px')
+x.style.display = "block"
+x.style.marginLeft = "auto"
+x.style.marginRight = "auto"
+
 function getComputerChoice() {
     let options = ['rock', 'paper', 'scissor']
-    let choice = Math.floor(Math.random() * 3)
-    return options[choice]
+    let choice = options[Math.floor(Math.random() * 3)]
+    let ui = choice.toString().toLowerCase()
+    let computerChoose = document.querySelector(".computerChoiceUI")
+    switch (ui) {
+        case 'rock':
+            x.setAttribute('src', './img/rock.png')
+            computerChoose.innerHTML = "";
+            computerChoose.appendChild(x)
+            return 'rock'
+        case 'paper':
+            x.setAttribute('src', './img/paper.png')
+            computerChoose.innerHTML = "";
+            computerChoose.appendChild(x)
+            return 'paper'
+        case 'scissor':
+            x.setAttribute('src', './img/scissors.png')
+            computerChoose.innerHTML = "";
+            computerChoose.appendChild(x)
+            return 'scissor'
+    }
 }
 
 function getHumanChoice() {
-    let userChoice = prompt("Enter your choice")
-    return userChoice.toString()
+    let menu = document.querySelector(".humanChoice")
+    let humanChoose = document.querySelector(".humanChoiceUI")
+    menu.addEventListener('click', (event) => {
+        let target = event.target
+        let choice = target.id
+        switch (target.id) {
+            case 'rock':
+                z.setAttribute('src', './img/rock.png')
+                break
+            case 'paper':
+                z.setAttribute('src', './img/paper.png')
+                break
+            case 'scissor':
+                z.setAttribute('src', './img/scissors.png')
+                break
+
+        }
+        humanChoose.innerHTML = "";
+        humanChoose.appendChild(z)
+        playRound(choice)
+    })
 }
 
-function playRound(humanChoice, computerChoice) {
-    let humanC = humanChoice
-    let computerC = computerChoice
-    let human = humanC.toString().toLowerCase()
-    let computer = computerC.toString().toLowerCase()
 
-    if (human === 'scissor' && computer === 'paper' || human === 'rock' && computer === 'scissor' || human === 'paper' && computer === 'rock') {
+function playRound(humanChoice) {
+    let computerChoice = getComputerChoice()
+    let score = document.querySelector(".scoreboard")
+    if (humanChoice === computerChoice) {
+        console.log("It's a tie!")
+    } else if (humanChoice === 'scissor' && computerChoice === 'paper' ||
+        humanChoice === 'rock' && computerChoice === 'scissor' ||
+        humanChoice === 'paper' && computerChoice === 'rock') {
         humanScore++
     } else {
         computerScore++
     }
+    score.textContent = `Computer: ${computerScore} | Human: ${humanScore}`
 }
+
 
 function playGame() {
-    for (let index = 0; index < 5; index++) {
-        playRound(getHumanChoice(), getComputerChoice)
-    }
-    if (humanScore > computerScore) {
-        console.log("Human you won")
-    } else {
-        console.log("You won AI")
-    }
+    getHumanChoice()
 }
 
-playGame();
+playGame()
